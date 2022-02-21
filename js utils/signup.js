c299b10d1d51c01c3366a95f76f859f367fb6e8e
Ali <Ola9047@gmail.com>
2022-02-21 03:12:01 +0200

    new version

--- js utils/signup.js
+++ js utils/signup.js
@@ -1,55 +1,55 @@
-function email() {
+document.getElementById("email").addEventListener('keyup', function () {
     if (document.getElementById("email").value.indexOf("@") == -1 || document.getElementById("email").value.indexOf(".com") == -1) {
         document.getElementById("emptyemail").innerHTML = "<span style='color: red; font-size:14px '> " + "Email does not contain a valid email." + "</span>"
     }
     else {
         document.getElementById("emptyemail").innerHTML = ""
     }
-}
+});
 
-function phone() {
+document.getElementById("phone").addEventListener('keyup', function () {
     let x = document.getElementById("phone").value
 
     if (x.length != 11 || x[0] != 0 || x[1] != 1 || (x[2] != 1 && x[2] != 2 && x[2] != 0))
         document.getElementById("emptyphone").innerHTML = "<span style='color: red; font-size:14px '> " + "This number is not valid according to your country." + "</span>"
     else
         document.getElementById("emptyphone").innerHTML = ""
-}
+});
 
 function rules() {
     document.getElementById("rules").innerHTML = "<span style = 'color:darkslategray ; font-size:12px ' >" + "Your password must have at-least 7 characters.<br>Your password must contain at - least 1 numeric character.<br>Spaces are not allowed in your password.<br>" + "</span>"
-    var x = document.getElementById("password").value
+    let x = document.getElementById("password2").value
     if (x.length < 7 || x.indexOf(" ") != -1 || (x.indexOf(0) == -1 && x.indexOf(1) == -1 && x.indexOf(2) == -1 && x.indexOf(3) == -1 && x.indexOf(4) == -1 && x.indexOf(5) == -1 && x.indexOf(6) == -1 && x.indexOf(7) == -1 && x.indexOf(8) == -1 && x.indexOf(9) == -1)) {
-        document.getElementById("emptypassword").innerHTML = "<span style='color: red; font-size:14px '> " + "The password does not satisfy the password policies." + "</span>"
+        document.getElementById("emptypassword2").innerHTML = "<span style='color: red; font-size:14px '> " + "The password does not satisfy the password policies." + "</span>"
     }
 
     else {
-        document.getElementById("emptypassword").innerHTML = ""
+        document.getElementById("emptypassword2").innerHTML = ""
     }
 }
 function add() {
     let x = document.getElementById("phone").value
-    let y = document.getElementById("password").value
+    let y = document.getElementById("password2").value
     let existUserName = false
 
-    if (document.getElementById("username").value == "") {
-        document.getElementById("emptyusername").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
+    if (document.getElementById("username2").value == "") {
+        document.getElementById("emptyusername2").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
     }
     if (document.getElementById("email").value == "") {
         document.getElementById("emptyemail").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
     }
-    if (document.getElementById("password").value == "") {
-        document.getElementById("emptypassword").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
+    if (document.getElementById("password2").value == "") {
+        document.getElementById("emptypassword2").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
     }
     if (document.getElementById("phone").value == "") {
         document.getElementById("emptyphone").innerHTML = "<span style='color: red; font-size:14px '> " + "This field must be not empty." + "</span>"
     }
-    if (document.getElementById("username").value != "" && document.getElementById("phone").value != "" && document.getElementById("email").value != "" && document.getElementById("email").value.indexOf("@") != -1 && document.getElementById("email").value.indexOf(".com") != -1 && document.getElementById("password").value.trim() != "" && (x.length == 11 && x[0] == 0 && x[1] == 1 && (x[2] == 1 || x[2] == 2 || x[2] == 0)) && y.length >= 7 && y.indexOf(" ") == -1 && (y.indexOf(0) != -1 || y.indexOf(1) != -1 || y.indexOf(2) != -1 || y.indexOf(3) != -1 || y.indexOf(4) != -1 || y.indexOf(5) != -1 || y.indexOf(6) != -1 || y.indexOf(7) != -1 || y.indexOf(8) != -1 || y.indexOf(9) != -1)) {
+    if (document.getElementById("username2").value != "" && document.getElementById("phone").value != "" && document.getElementById("email").value != "" && document.getElementById("email").value.indexOf("@") != -1 && document.getElementById("email").value.indexOf(".com") != -1 && document.getElementById("password2").value.trim() != "" && (x.length == 11 && x[0] == 0 && x[1] == 1 && (x[2] == 1 || x[2] == 2 || x[2] == 0)) && y.length >= 7 && y.indexOf(" ") == -1 && (y.indexOf(0) != -1 || y.indexOf(1) != -1 || y.indexOf(2) != -1 || y.indexOf(3) != -1 || y.indexOf(4) != -1 || y.indexOf(5) != -1 || y.indexOf(6) != -1 || y.indexOf(7) != -1 || y.indexOf(8) != -1 || y.indexOf(9) != -1)) {
 
         const acc = JSON.parse(localStorage.getItem("accounts")) || []
 
         for (var i = 0; i < acc.length; i++) {
-            if (document.getElementById("username").value == acc[i].username) {
+            if (document.getElementById("username2").value == acc[i].username) {
                 existUserName = true;
                 break;
             }
@@ -64,37 +64,37 @@ function add() {
             var accounts = JSON.parse(localStorage.getItem("accounts")) || []
 
             accounts.push({
-                username: document.getElementById("username").value,
+                username: document.getElementById("username2").value,
                 email: document.getElementById("email").value,
-                password: document.getElementById("password").value,
+                password: document.getElementById("password2").value,
                 phone: document.getElementById("phone").value
             })
             localStorage.setItem("accounts", JSON.stringify(accounts))
 
 
-            localStorage.setItem("document.getElementById('user')", document.getElementById("username").value)
+            localStorage.setItem("document.getElementById('user')", document.getElementById("username2").value)
             location.href = "jewellery.html"
         }
     }
 }
 
-document.getElementById("password").addEventListener('keyup', function () {
-    if (document.getElementById("password").value === "") {
-        document.getElementById("eye").style.visibility = "hidden"
+document.getElementById("password2").addEventListener('keyup', function () {
+    if (document.getElementById("password2").value === "") {
+        document.getElementById("eye2").style.visibility = "hidden"
     }
     else {
-        document.getElementById("eye").style.visibility = "visible"
+        document.getElementById("eye2").style.visibility = "visible"
     }
 });
 
-document.getElementById("eye").addEventListener('click', function () {
-    if (document.getElementById("password").type === "password") {
-        document.getElementById("password").type = "text"
-        document.getElementById("eye").classList.toggle('bi-eye')
+document.getElementById("eye2").addEventListener('click', function () {
+    if (document.getElementById("password2").type === "password") {
+        document.getElementById("password2").type = "text"
+        document.getElementById("eye2").classList.toggle('bi-eye')
     }
     else {
-        document.getElementById("password").type = "password"
-        document.getElementById("eye").classList.toggle('bi-eye')
+        document.getElementById("password2").type = "password"
+        document.getElementById("eye2").classList.toggle('bi-eye')
     }
 });
 
